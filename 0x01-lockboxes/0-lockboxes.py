7c8cb82f611c80ec4cc9fe949cda5cabaad0c6a5
kwenziwa <kwenziwa@live.com>
2023-12-05 20:57:02 +0000

    bug fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,65 @@
 #!/usr/bin/python3
-def canUnlockAll(boxes):
-    if not boxes or not boxes[0]:
-        return False
+"""Solves the lock bxs puzzle """
 
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-    stack = [0]
 
-    while stack:
-        current_box = stack.pop()
+def my_next(opn_bx):
+    """Looks for the next opened box
+    Args:
+        opn_bx (dict): Dictionary which contains bxs already opened
+    Returns:
+        list: List with the keys contained in the opened box
+    """
+    for index, box in opn_bx.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
 
-        for key in boxes[current_box]:
-            if 0 <= key < n and not visited[key]:
-                visited[key] = True
-                stack.append(key)
 
-    return all(visited)
+def canUnlockAll(bxs):
+    """Check if all bxs can be opened
+    Args:
+        bxs (list): List which contain all the bxs with the keys
+    Returns:
+        bool: True if all bxs can be opened, otherwise, False
+    """
+    if len(bxs) <= 1 or bxs == [[]]:
+        return True
+
+    an_au = {}
+    while True:
+        if len(an_au) == 0:
+            an_au[0] = {
+                'status': 'opened',
+                'keys': bxs[0],
+            }
+        keys = my_next(an_au)
+        if keys:
+            for key in keys:
+                try:
+                    if an_au.get(key) and an_au.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    an_au[key] = {
+                        'status': 'opened',
+                        'keys': bxs[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in an_au.values()]:
+            continue
+        elif len(an_au) == len(bxs):
+            break
+        else:
+            return False
+
+    return len(an_au) == len(bxs)
+
+
+def main():
+    """Entry point"""
+    canUnlockAll([[]])
+
+
+if __name__ == '__main__':
+    main()
